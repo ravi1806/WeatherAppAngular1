10bc4b1b9c7b3992644c3c85f8de74eed40292dc
Ravi Yadav <raviyadav1806@gmail.com>
2017-02-28 22:36:33 +0530

    Getting service out of controller

--- controllers.js
+++ controllers.js
@@ -18,23 +18,14 @@ weatherApp.controller("HomeCtrl",['$scope','cityService','$location',function($s
 	
 }]);
 
-weatherApp.controller("ForecastCtrl",['$scope','cityService','$resource','$routeParams',function($scope,cityService,$resource,$routeParams){
+weatherApp.controller("ForecastCtrl",['$scope','cityService','$routeParams','weatherService',function($scope,cityService,$routeParams,weatherService) {
 	//As we have diffr $scope for each controller.
     
 	
 	$scope.city = cityService.city;
 	$scope.days = $routeParams.days || '2'; //routeParams value is string and not number.
-	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',{get: {method: 'JSONP'}});
-	$scope.weatherResult = $scope.weatherAPI.get( 
-												  { 
-													q: $scope.city,
-													cnt: $scope.days,
-													APPID: '6f6d395cd7089e9b23c0940ecf01973f'
-												  }
-												);
-	
-	//console.log($scope.weatherResult);
-	
+
+	$scope.weatherResult = weatherService.GetWeather($scope.city,$scope.days); 
 	//Convert to celsius
 	$scope.convertToCelsius = function(degK) {
 	   return (degK-273.15).toFixed(2);
--- services.js
+++ services.js
@@ -2,3 +2,20 @@
 weatherApp.service('cityService',function(){
 	this.city = 'Mumbai, MH';
 });
+
+weatherApp.service('weatherService',['$resource',function($resource){
+	
+	this.GetWeather = function(city,days) {
+	
+	var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',{get: {method: 'JSONP'}});
+	return  weatherAPI.get( 
+								  { 
+									q: city,
+									cnt: days,
+									APPID: '6f6d395cd7089e9b23c0940ecf01973f'
+								  }
+							  );
+			 
+	//console.log($scope.weatherResult);
+  };
+}]);
